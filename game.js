63557cb58099e37d18d8bb922543e9d531f61e06
Evan03 <Evan.onihotmail.com>
2023-04-14 15:09:11 -0400

    allowed display of board

--- game.js
+++ game.js
@@ -1,35 +1,46 @@
-
-// this module controls the game board
-const gameBoard =(()=>{
- let board = ['', '', '', '', '', '', '', '',]
-
-function getBoard() {
-    return board
-}
-
-
-function resetBoard(){
-    return board =['', '', '', '', '', '', '', '','']
-}
-
-
-function placeMarker(){
-    
-}
-})
+const gameBoard=(function(){
+    //creates a game board with 9 clots
+   const board=["X", "O", "X", "O", "O", "X", "O", "X", "X"]
 
 
 
 
+   
+
+   const renderBoard=(board)=>{
+      let render = document.querySelectorAll('.cell')
+      render.forEach((render, index)=>{
+            render.innerHTML= board[index];
+      }
+)}
+
+
+   return{
+    board,
+     renderBoard
+   }
+})();
+
+gameBoard.renderBoard(gameBoard.board);
+
+// players object
+
+
+const Player =(name, marker) => {
+
+    const getName=()=>name;
+    const getMarker =()=>marker;
+
+   return {getName, getMarker}
+};
 
 
 
 
-// This factory function creates the players and sets their markers 
-const players =((name, marker)=>{
-    
-    return{
-        name:name,
-        marker:marker
-    }
-})
+
+// Method that controls the game board
+
+const runGame=(function(){
+
+})();
+
