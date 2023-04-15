3254a63c6c6e7d4a39c01732359589852e17bf8c
Evan03 <Evan.onihotmail.com>
2023-04-14 23:40:52 -0400

    allowed for player to append their marker to grid

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const gameBoard=(function(){
     //creates a game board with 9 clots
-   const board=["X", "O", "X", "O", "O", "X", "O", "X", "X"]
+   const board=["", "", "", "", "", "", "", "", ""]
 
 
 
@@ -8,11 +8,14 @@ const gameBoard=(function(){
    
 
    const renderBoard=(board)=>{
-      let render = document.querySelectorAll('.cell')
+      const render = document.querySelectorAll('.cell')
       render.forEach((render, index)=>{
             render.innerHTML= board[index];
       }
 )}
+
+
+
 
 
    return{
@@ -23,24 +26,45 @@ const gameBoard=(function(){
 
 gameBoard.renderBoard(gameBoard.board);
 
+
+// running the game
+const gameControl = (() => {
+    // game control logic
+  
+
+
+    
+    const updateBoard = (marker, index) => {
+      gameBoard.getBoard()[index] = marker;
+    };
+  
+    return {  updateBoard };
+  })();
+
 // players object
 
 
 const Player =(name, marker) => {
+// selects all the cells and appends the players marker to it 
+ const cell = document.querySelectorAll(".cell");
+  cell.forEach((cell, index) => {
+    cell.addEventListener("click", () => {
+      cell.innerHTML = getMarker();
+      gameControl.updateBoard(getMarker(), index);
+    });
+  });
 
     const getName=()=>name;
     const getMarker =()=>marker;
-
    return {getName, getMarker}
 };
 
 
-
+const Evan = Player('Evan', "X");
+const Jason = Player('Jason', "O")
 
 
 // Method that controls the game board
 
-const runGame=(function(){
 
-})();
 
